faa24e6536b313965205d1efa6eeca29f30a1610
Adrian <adrianginergimenez@gmail.com>
2024-08-02 15:45:32 +0200

    fix: comment format

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+//Coded by Adrian Giner Gimenez. FullStackWebpage. 02-08-2024. By humans for humans.
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
